e0777ea7aaec53f3d47025549e035fe7e9e08fc1
Duc Nguyen <ndpduc@gmail.com>
2018-06-28 00:05:33 +0700

    - Update fastlane script.

--- itunes/populate_metadata.py
+++ itunes/populate_metadata.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = "python ../../../store-listing-toolkit/populate-v2.py metadata -platform iOS -prj-path . -sheet 1oqexrvD9EjAonXGnclKRvyD9HtgomWhWKmj-yc3uzq8 -customized-metadata-path ../src/itunes/metadata"
+cmd = "python ../../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 175xgn3mdWMbjSkPBz2sir1z_YndHM0A1ia5efnJ4_Tg -customized-metadata-path ../src/itunes/metadata"
 print cmd
 os.system(cmd)
