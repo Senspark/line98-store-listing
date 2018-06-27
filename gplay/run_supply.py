94242834a521de4bb5bc874143c1bd5bb2e54101
Duc Nguyen <ndpduc@gmail.com>
2018-06-27 18:19:20 +0700

    - Update scripts

--- gplay/run_supply.py
+++ gplay/run_supply.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = 'supply -j ../../Google\ Play\ Android\ Developer-394f15d83386.json -p com.senspark.lines98 --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
+cmd = 'fastlane supply -j ../../Google_Play_Android_Developer.json -p com.senspark.lines98 --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
 print cmd
 os.system(cmd)
